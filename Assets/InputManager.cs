741c611be1f016512d055c024dc089db9e9c3385
Maximilian Csuk <maximilian.csuk@gmx.at>
2019-10-03 17:38:01 +0200

    Implemented crude input handling for attractors

---
+++ Assets/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    bool isDragging = false;
+    GameObject draggingObject;
+
+    public void Update()
+    {
+        Input.simulateMouseWithTouches = true; // HACK
+        if (Input.GetMouseButton(0))
+        {
+            if (!isDragging)
+            {
+                draggingObject = GetObjectFromMouseRaycast();
+                if (draggingObject)
+                {
+                    isDragging = true;
+                }
+            }
+            else if (draggingObject != null)
+            {
+                var targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                targetPosition.z = 0f;
+                draggingObject.transform.position = targetPosition;
+            }
+        }
+        else
+        {
+            isDragging = false;
+        }
+    }
+
+    private GameObject GetObjectFromMouseRaycast()
+    {
+        GameObject gmObj = null;
+        var collider = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if (collider != null)
+        {
+            gmObj = collider.gameObject;
+        }
+        return gmObj;
+    }
+}
