c108eff61316832719cd59a31d39e91f86ef63ff
Maximilian Csuk <maximilian.csuk@gmx.at>
2019-10-03 11:28:01 +0200

    work on attractors

--- Assets/Attractor.cs
+++ Assets/Attractor.cs
@@ -7,16 +7,23 @@ public class Attractor : MonoBehaviour {
     public float maxRange = 4;
     public float strength = 1;
 
+    public AnimationCurve forceCurve;
+
     public bool repulsor = false;
 
     public Vector3 GetAttractDir(Vector3 initialPos) {
         Vector3 result = transform.position - initialPos;
         float dist = result.magnitude;
         float force = 1 - Mathf.Pow(Mathf.Clamp01((dist - minRange) / (maxRange - minRange)), 2);
+
+        force = forceCurve.Evaluate(force);
+        //force = Mathf.SmoothStep(0, 1, force);
+
         force *= strength;
         if (repulsor) {
             force *= -1;
         }
+
         result.Normalize();
         result *= force;
         return result;
